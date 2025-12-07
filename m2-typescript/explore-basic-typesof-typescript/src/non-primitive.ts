6c1193e74ce799955eb3655c1fb9976267694c24
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-12-07 22:12:07 +0600

    Object, Literal & Optional Type

--- m2-typescript/explore-basic-typesof-typescript/src/non-primitive.ts
+++ m2-typescript/explore-basic-typesof-typescript/src/non-primitive.ts
@@ -2,7 +2,7 @@
 // TS: tuple
 let bazarList: string[] = ['apple', 'banana', 'milk', 'bread'];
 
-bazarList.push(12); // Error: Argument of type 'number' is not assignable to parameter of type 'string'
+// bazarList.push(12); // Error: Argument of type 'number' is not assignable to parameter of type 'string'
 bazarList.push('eggs'); // OK
 
 let mixedArr: (number | string | boolean)[] = [12, 'hello', true];
@@ -22,6 +22,45 @@ let couple : [string, string] = ['Husband', 'Wife'];
 couple = ['Father', 'Mother']; // OK
 // couple = ['Brother', 123]; // Error: Type 'number' is not assignable to type 'string'
 
-let nameRole: [string, string, number]; = ['Sabbir Niyaz', 'Admin', 1];
+let nameRole: [string, string, number] = ['Sabbir Niyaz', 'Admin', 1];
 nameRole = ['John Doe', 'User', 2]; // OK
 // nameRole = ['Jane Doe', 3, 'Moderator']; // Error: Type 'number' iss not assignable to type 'string'
+
+
+// reference type: object
+
+const user:{
+    // organization: string;
+    organization: 'Programming Hero'; // Literal type (Fixed value type)
+    firstName: string;
+    middleName?: string; // optional type or property
+    lastName?: string; // optional type or property
+    isAdmin: boolean;
+} = {
+    organization: 'Programming Hero',
+    firstName: 'Sabbir',
+    // middleName: 'Hossain',
+    lastName: 'Niyaz',
+    isAdmin: true
+}
+
+user.firstName = 'John'; // OK
+// user.age = 30; // Error: Property 'age' does not exist on type '{ organization: string; firstName: string; middleName?: string | undefined; lastName?: string | undefined; isAdmin: boolean; }'
+// user.isAdmin = 'yes'; // Error: Type 'string' is not assignable to type 'boolean'
+
+console.log(user);
+
+const user2:{
+    // organization: string;
+    readonly organization: 'Programming Hero'; // access modifier: readonly
+    firstName: string;
+    middleName?: string; // optional type or property
+    lastName?: string; // optional type or property
+    isAdmin: boolean;
+} = {
+    organization: 'Programming Hero',
+    firstName: 'Sabbir',
+    // middleName: 'Hossain',
+    lastName: 'Niyaz',
+    isAdmin: true
+}
