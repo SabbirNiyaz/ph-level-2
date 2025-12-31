4ae8fe6455f3c55abc79da5662b9bcf7dbd03c31
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2025-12-31 23:11:42 +0600

    Problem 2 done

--- m2-typescript/assignment/src/p2.ts
+++ m2-typescript/assignment/src/p2.ts
@@ -1,1 +1,13 @@
 //todo-> Problem 2:
+
+type InputType = string | number[];
+const getLength = (input : InputType) => {
+    if (typeof input === 'string') {
+        return input.length;
+    } else if (Array.isArray(input)) {
+        return input.length;
+    } 
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
