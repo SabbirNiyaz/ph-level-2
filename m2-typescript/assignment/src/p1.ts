ac717803164ffcad6fea5a4c0d04f07644aca234
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2025-12-31 23:04:24 +0600

    Problem 1 done

---
+++ m2-typescript/assignment/src/p1.ts
@@ -0,0 +1,19 @@
+//todo-> Problem 1:
+
+type ValueType = string | number | boolean
+
+const formatValue = (value: ValueType) => {
+    if (typeof value === 'string') {
+        return value.toUpperCase()
+    } else if (typeof value === 'number') {
+        return value * 10;
+    } else {
+        return !value;
+    }
+}
+
+console.log(formatValue('hello'));
+console.log(formatValue(5));
+console.log(formatValue(true));
+
+
