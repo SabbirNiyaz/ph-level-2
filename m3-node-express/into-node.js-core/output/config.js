648cb5fb60b90ffc365426db863946637e9b932b
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-01-25 19:42:58 +0600

    Using dotenv with Configuration

---
+++ m3-node-express/into-node.js-core/output/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+    app: {
+        name: process.env.APP_NAME || 'DefaultApp',
+        version: process.env.APP_VERSION || '1.0.1',
+        port: process.env.PORT || 3000,
+        environment: process.env.NODE_ENV || 'Web Development'
+    }
+};          
+ 
+console.log("Application Configuration: ", config.app);
+
+module.exports = config;
