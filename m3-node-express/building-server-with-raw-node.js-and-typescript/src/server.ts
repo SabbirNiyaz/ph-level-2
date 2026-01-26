61006566c01d8aa0947262e18cc9423c23c8152f
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-01-26 19:46:48 +0600

    Custom Route Handler

--- m3-node-express/building-server-with-raw-node.js-and-typescript/src/server.ts
+++ m3-node-express/building-server-with-raw-node.js-and-typescript/src/server.ts
@@ -1,69 +1,96 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
 import config from './config';
+import addRoutes, { RouteHandler, routes } from './helpers/RouteHandler';
 
-const server: Server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
-    console.log("Server received a request.");
-
-    // ======================
-    // ROOT ROUTE
-    // ======================
-    if (req.url === '/' && req.method === 'GET') {
+addRoutes('GET', '/', (req, res) => {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       return res.end(JSON.stringify({
         message: 'Hello, NodeJS!!',
         path: req.url
       }));
+
+});
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("Server received a request.");
+
+    const method = req.method?.toUpperCase() || '';
+    const path = req.url || '';
+
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+
+    if(handler){
+        return handler(req, res);
+    }else{
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({
+          success: false,
+          message: 'Route not found',
+          path,
+        }));
     }
+
+    // ======================
+    // ROOT ROUTE
+    // ======================
+    // if (req.url === '/' && req.method === 'GET') {
+    //   res.writeHead(200, { 'Content-Type': 'application/json' });
+    //   return res.end(JSON.stringify({
+    //     message: 'Hello, NodeJS!!',
+    //     path: req.url
+    //   }));
+    // }
 
     // ======================
     // HEALTH ROUTE
     // ======================
-    if (req.url === '/api' && req.method === 'GET') {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      return res.end(JSON.stringify({
-        message: 'Health Status: OK',
-        path: req.url
-      }));
-    }
+  //   if (req.url === '/api' && req.method === 'GET') {
+  //     res.writeHead(200, { 'Content-Type': 'application/json' });
+  //     return res.end(JSON.stringify({
+  //       message: 'Health Status: OK',
+  //       path: req.url
+  //     }));
+  //   }
 
-    // ======================
-    // USER ROUTE
-    // ======================
-    if (req.url === '/api/users' && req.method === 'POST') {
-      let body = '';
+  //   // ======================
+  //   // USER ROUTE
+  //   // ======================
+  //   if (req.url === '/api/users' && req.method === 'POST') {
+  //     let body = '';
 
-      // Listen for incoming data
-      req.on('data', chunk => {
-        body += chunk.toString();
-      });
+  //     // Listen for incoming data
+  //     req.on('data', chunk => {
+  //       body += chunk.toString();
+  //     });
 
-      // When data is fully received
-      req.on('end', () => {
-        try{
-         const parsedBody = JSON.parse(body);
-         console.log('Received user data:', parsedBody);
+  //     // When data is fully received
+  //     req.on('end', () => {
+  //       try{
+  //        const parsedBody = JSON.parse(body);
+  //        console.log('Received user data:', parsedBody);
             
-         res.writeHead(200, { 'Content-Type': 'application/json' });
-         res.end(JSON.stringify({
-         message: 'User data received successfully!!',
-         data: parsedBody
-        }));
+  //        res.writeHead(200, { 'Content-Type': 'application/json' });
+  //        res.end(JSON.stringify({
+  //        message: 'User data received successfully!!',
+  //        data: parsedBody
+  //       }));
 
-        }catch(err: any){
-            console.log(err?.message);
-      }
-      }
-    );
+  //       }catch(err: any){
+  //           console.log(err?.message);
+  //     }
+  //     }
+  //   );
 
-      return;
-    }
+  //     return;
+  //   }
 
-    // ======================
-    // NOT FOUND ROUTE
-    // ======================
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Route not found' }));
+  //   // ======================
+  //   // NOT FOUND ROUTE
+  //   // ======================
+  //   res.writeHead(404, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify({ message: 'Route not found' }));
   }
 );
 
