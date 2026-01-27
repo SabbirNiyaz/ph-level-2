74da0c6b6d03184b1b54b754dcdad1cb13061834
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-01-27 23:42:35 +0600

    Express CRUD Operation Done

--- m3-node-express/crud-with-express-typescript-and-postgresql/src/server.ts
+++ m3-node-express/crud-with-express-typescript-and-postgresql/src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({path: path.join(__dirname, '../.env')});
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const app = express()
 const port = 5000
@@ -45,20 +45,186 @@ const initDb = async () => {
 
 initDb();
 
+//? logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+    next();
+}
 
-app.get('/', (req: Request, res: Response) => {
+
+app.get('/', logger, (req: Request, res: Response) => {
     res.send('Hello Express!')
 })
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
+//! users CRUD Operations
+//todo: Create User 
+app.post('/users/create', async (req: Request, res: Response) => {
+    // console.log(req.body);
+    const { name, email, age } = req.body;
 
-    res.status(201).json({
-        success: true,
-        message: 'Data created successfully',
-        data: req.body
-    })
+    try {
+        const result = await pool.query(
+            `INSERT INTO users (name, email, age) 
+            VALUES ($1, $2, $3) RETURNING *`,
+            [name, email, age]
+        )
+        // console.log(result.rows[0]);
+
+        res.status(201).json({
+            message: 'Data Inserted',
+            data: result.rows[0]
+        })
+
+    } catch (err: any) {
+        console.error('Error creating user:', err);
+        return res.status(500).json({
+            success: false,
+            message: 'Error creating user.',
+            error: err.message
+        })
+    }
 })
+
+//todo: Get All Users
+app.get('/users', logger, async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.status(200).json({
+            success: true,
+            data: result.rows
+        })
+
+    } catch (err: any) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error fetching users.',
+            error: err.message
+        })
+    }
+});
+
+//todo: Get Users by ID
+app.get('/users/:id', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`,
+            [req.params.id]);
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'User not found.'
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                data: result.rows
+            })
+        }
+        // console.log(result.rows);
+
+    } catch (err: any) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error fetching users.',
+            error: err.message
+        })
+    }
+});
+
+//todo: Update User by ID
+app.put('/users/update/:id', async (req: Request, res: Response) => {
+    const { name, email, age } = req.body;
+    try {
+        const result = await pool.query(`
+            UPDATE users 
+            SET name=$1, email=$2, age=$3 
+            WHERE id=$4 RETURNING *`,
+            [name, email, age, req.params.id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'User not found.'
+            })
+        } else {
+            res.status(201).json({
+                success: true,
+                data: result.rows
+            })
+        }
+        // console.log(result.rows);
+
+    } catch (err: any) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error updating users.',
+            error: err.message
+        })
+    }
+});
+
+//todo: Delete User by ID
+app.delete('/users/delete/:id', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`,
+            [req.params.id]);
+        if (result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'User not found.'
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: 'User deleted successfully.',
+                data: null
+            })
+        }
+
+    } catch (err: any) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error deleting users.',
+            error: err.message
+        })
+    }
+});
+
+//! todos CRUD Operations
+//todo: Create Todo
+app.post('/todos/create', async (req: Request, res: Response) => {
+    // console.log(req.body);
+    const { user_id, phone, address, completed, due_date } = req.body;
+
+    try {
+        const result = await pool.query(
+            `INSERT INTO todos (user_id, phone, address, completed, due_date) 
+            VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [user_id, phone, address, completed, due_date]
+        )
+
+        res.status(201).json({
+            message: 'Data Inserted',
+            data: result.rows[0]
+        })
+
+    } catch (err: any) {
+        console.error('Error creating todo:', err);
+        return res.status(500).json({
+            success: false,
+            message: 'Error creating todo.',
+            error: err.message
+        })
+    }
+})
+
+//todo: Not Found Route Handler
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found.',
+        path: req.path
+    })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
